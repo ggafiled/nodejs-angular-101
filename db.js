bb3e256cba5184ac8386abb3125f734e088ae3e4
unknown <gafewkik234@gmail.com>
2019-05-30 14:46:46 +0700

    make it better

--- db.js
+++ db.js
@@ -2,7 +2,7 @@
  * ไฟล์สำหรับเชื่อมต่อฐานข้อมูล MongooseDB 
  */
 const mongoose = require('mongoose');
-const url = 'mongodb://localhost:27017/workshop';
+const url = process.env.MONGODB_URI;
 
 mongoose.connect(url,(err)=>{
     if (err){
